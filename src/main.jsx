6bfcbce42b20da37124e0ac2579b4a550b7bb436
ibrarMalick <ibrar@deazitech.com>
2024-01-24 13:07:23 +0500

    Routing is done

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,19 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store.js';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Details, PageNotFound } from './pages';
+
+const router = createBrowserRouter([
+	{ path: '/', element: <App /> },
+	{ path: '/details', element: <Details /> },
+	{ path: '*', element: <PageNotFound /> },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<>
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	</>,
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Details from './Details';
 import Home from './Home';
+import PageNotFound from './PageNotFound';
 
-export { Home, Details };
+export { Home, Details, PageNotFound };
